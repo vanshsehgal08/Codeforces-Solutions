2a7f10d5bbf46b1e3fb62547c662ccee36e1e260
Vansh Sehgal <116642928+vanshsehgal08@users.noreply.github.com>
2025-09-12 22:53:49 +0530

    Add Ambitious Kid [A] from Codeforces

---
+++ 1866/A - Ambitious Kid/solution.java
@@ -0,0 +1,1 @@
+import java.util.*; public class Main {    public static void main(String[] args) {                Scanner sc=new Scanner(System.in);        int n=sc.nextInt();        int ans=Integer.MAX_VALUE;                for (int i=0; i<n; i++) {            int x=sc.nextInt();            ans=Math.min(ans, Math.abs(x));        }        System.out.println(ans);    }}
